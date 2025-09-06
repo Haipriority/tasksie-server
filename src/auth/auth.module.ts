a17e435db816ba75232661aec68b7e6ae2fb313b
hai-dlaboy <dlaboy@haipriori.com>
2025-09-06 10:38:18 -0400

    7 bugs found

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
-import { Module } from '@nestjs/common';
+// src/auth/auth.module.ts
+import { Module, forwardRef } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { User } from '../user/entities/user.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [
+    TypeOrmModule.forFeature([User]),  // <-- THIS provides Repository<User> in this module
+    forwardRef(() => UserModule),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET!,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  providers: [AuthService, JwtAuthGuard],
+  exports: [AuthService, JwtModule, JwtAuthGuard],
 })
 export class AuthModule {}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { Task } from './entities/task.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  controllers: [TasksController],
+  imports: [
+    TypeOrmModule.forFeature([Task]),
+    forwardRef(() => AuthModule), // brings in JwtService + guard
+  ], // <-- provides Repository<Task>
   providers: [TasksService],
+  controllers: [TasksController],
+  exports: [TasksService], // if other modules need it
 })
 export class TasksModule {}
